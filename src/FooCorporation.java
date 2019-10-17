98be5a18ab9c207ab116cfd1e43b782224d8b287
udevarap <dudaykumarreddy91@gmail.com>
2019-10-17 17:31:54 -0400

    sample

---
+++ src/FooCorporation.java
@@ -0,0 +1,31 @@
+public class FooCorporation
+{
+
+    public static void main(String[] args) {
+        FC(8.20,47);
+    }
+
+
+
+    public static void FC(double pay, int hours)
+    {
+        double totalPay = pay*hours;
+        System.out.println("Total pay of the employee is " + totalPay);
+
+        if (pay==7.50 && (hours<=35){
+            System.out.println(" this is employee1");
+
+        } else if (pay==8.20 && hours==47) {
+            System.out.println(" this is employee2");
+        }
+
+            else if (pay==10.00 && hours==73) {
+            System.out.println(" this is employee3");
+        }else if(pay>40){
+               double overtime  =  (pay)* 1.5;
+            System.out.println(" he works" + "which is overtime");
+        }
+            System.out.println( -1);
+        }
+    }
+
